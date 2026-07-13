b56c42915f24fccc979074d6efc8bea927b3a472
Andry Rakotomanana <tahina.andry@gmail.com>
2026-07-13 18:47:55 +0300

    Remove unused homepage imports and CTA component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import AboutPreview from "@/components/AboutPreview";
 import ServicesPreview from "@/components/ServicesPreview";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import ProjectsPreview from "@/components/ProjectsPreview";
-import ContactCTA from "@/components/ContactCTA";
 import Footer from "@/components/Footer";
 import CallToAction from "@/components/CallToAction";
 
